747f1de0920050f68ab453e64bd2e60bd270a17f
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-02 10:16:30 -0700

    starts create react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+const reactContentRoot = document.getElementById("root");
+
+const App = () => {
+	const myItem = "Ekene";
+
+	return (
+		<ul>
+			<li>item1</li>
+			<li>item255 {myItem}</li>
+			<li>{myItem.toUpperCase()}</li>
+		</ul>
+	);
+};
+
+ReactDOM.render(<App />, reactContentRoot);
