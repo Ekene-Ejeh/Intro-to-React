dc27bc243a74002ff01ee6360469749eff8527b9
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-05 09:50:24 -0700

    learnt how to style conponents and also styling with props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,9 +4,9 @@ import CountBtn from "./CountBtn";
 const App = () => {
 	return (
 		<div>
-			<CountBtn incrementBy={1} />
-			<CountBtn incrementBy={5} />
-			<CountBtn incrementBy={8} />
+			<CountBtn incrementBy={1} btnColor={"red"} borderRadius={"2px"} />
+			<CountBtn incrementBy={5} btnColor={"green"} borderRadius={"10px"} />
+			<CountBtn incrementBy={8} btnColor={"blue"} borderRadius={"15px"} />
 		</div>
 	);
 };
--- src/components/CountBtn.jsx
+++ src/components/CountBtn.jsx
@@ -9,9 +9,25 @@ const CountBtn = (props) => {
 		setCurrentCount(currentCount + props.incrementBy);
 	};
 
+	const divStyle = {
+		color: "red",
+		border: "1px solid blue",
+		borderRadius: "12px",
+		padding: "10px",
+	};
+
+	const btnStyles = {
+		background: props.btnColor,
+		borderRadius: props.borderRadius,
+		padding: "10px",
+		margin: "5px",
+	};
+
 	return (
-		<div>
-			<button onClick={handleClick}>+{props.incrementBy}</button>
+		<div style={divStyle}>
+			<button style={btnStyles} onClick={handleClick}>
+				+{props.incrementBy}
+			</button>
 			<div>{currentCount}</div>
 		</div>
 	);
