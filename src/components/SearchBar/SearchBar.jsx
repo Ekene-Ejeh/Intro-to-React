c919a68db2d5cd99eb2803a9099a3570edd53db8
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-05 13:26:40 -0700

    using the map fuction to display products on the browser

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,28 @@
 import React, { useState } from "react";
 import "./SearchBar.css";
 
+const products = [
+	"Tooth paste",
+	"Tooth brush",
+	"Mouth wash",
+	"Dental floss",
+	"Mouth Guard",
+];
+
+// storing the input in a state
 const SearchBar = () => {
 	const [searchValue, setSearchValue] = useState("");
 
+	// displaying the input when changed
 	const handleInputChange = (event) => {
 		setSearchValue(event.target.value);
 	};
 
+	// clearing the input
 	const handleClearBtn = () => {
 		setSearchValue("");
 	};
-
+	// conditional rendering on the clear btn
 	const shouldDisplayClearBtn = searchValue.length > 0;
 
 	return (
@@ -21,6 +32,10 @@ const SearchBar = () => {
 			{shouldDisplayClearBtn && (
 				<button onClick={handleClearBtn}>Clear Input</button>
 			)}
+
+			{products.map((product) => {
+				return <li>{product}</li>;
+			})}
 		</div>
 	);
 };
